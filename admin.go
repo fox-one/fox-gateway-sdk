44b197d56a570dbe5c32aed675b422385fa60b7e
Charlie Wu <wu@fox.one>
2019-01-18 14:55:00 +0800

    MOD: add admin validate;

--- admin.go
+++ admin.go
@@ -1,1 +1,46 @@
 package gateway
+
+import (
+	"context"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+// Admin admin
+type Admin struct {
+	*Client
+}
+
+// NewAdmin new admin
+func NewAdmin(apiBase string) *Admin {
+	return &Admin{
+		Client: NewClient(apiBase),
+	}
+}
+
+// Validate validate
+func (a *Admin) Validate(ctx context.Context, method, uri, body, token string) (*AdminUserView, error) {
+	data, err := a.POST("/admin/validate").
+		P("method", method).
+		P("uri", uri).
+		P("body", body).
+		P("token", token).
+		Do(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp struct {
+		Err
+		AdminUser *AdminUserView `json:"admin"`
+	}
+	if err := jsoniter.Unmarshal(data, &resp); err != nil {
+		return nil, err
+	}
+
+	if resp.Code > 0 {
+		return nil, resp.Err
+	}
+
+	return resp.AdminUser, nil
+}
--- view.go
+++ view.go
@@ -1,14 +1,24 @@
 package gateway
 
+// MemberView member
 type MemberView struct {
 	ID        string `json:"id"`
 	CreatedAt int64  `json:"created_at"`
 	IsPinSet  bool   `json:"is_pin_set"`
 }
 
+// MemberSessionView member session
 type MemberSessionView struct {
 	Key       string `json:"key"`
 	Secret    string `json:"secret"`
 	CreatedAt int64  `json:"created_at"`
 	ExpiredAt int64  `json:"expired_at"`
+}
+
+// AdminUserView admin user
+type AdminUserView struct {
+	ID        uint   `json:"id"`
+	CreatedAt int64  `json:"created_at"`
+	Username  string `json:"username"`
+	Merchant  string `json:"merchant"`
 }
