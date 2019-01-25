786bc57081b2c0b7d90dc5670f602c9365833361
Zhiming Jiang <lanvige@gmail.com>
2019-01-25 22:48:45 +0800

    update wallet

--- wallet.go
+++ wallet.go
@@ -22,12 +22,43 @@ func (asset Asset) Error() string {
 	return asset.AssetID
 }
 
-// AssetList xx
+// AssetList model
 type AssetList []Asset
 
-// Implements the error interface
+// Implements the AssetList error interface
 func (assetList AssetList) Error() string {
-	return "asset"
+	return "asset list error"
+}
+
+// Snapshot model
+type Snapshot struct {
+	SnapshotID string      `json:"snapshot_id"`
+	TraceID    string      `json:"trace_id"`
+	WalletID   string      `json:"wallet_id"`
+	AssetID    string      `json:"asset_id"`
+	OpponentID string      `json:"opponent_id"`
+	Source     string      `json:"source"`
+	Amount     string      `json:"amount"`
+	Memo       string      `json:"memo"`
+	MemberID   string      `json:"member_id"`
+	Service    string      `json:"service"`
+	Label      string      `json:"label"`
+	Data       interface{} `json:"data"`
+	CreatedAt  int64       `json:"created_at"`
+	Asset      Asset       `json:"asset"`
+}
+
+// Implements the error interface
+func (snapshot Snapshot) Error() string {
+	return "snapshot error"
+}
+
+// SnapshotList model
+type SnapshotList []Snapshot
+
+// Implements the AssetList error interface
+func (snapshotList SnapshotList) Error() string {
+	return "snapshot list error"
 }
 
 // Wallet Model
@@ -43,8 +74,16 @@ func (wallet Wallet) Error() string {
 	return wallet.Label
 }
 
+// WalletList model
+type WalletList []Wallet
+
+// Implements the error interface
+func (walletList WalletList) Error() string {
+	return "wallet list error"
+}
+
 // GetAssets 取用户的资产
-func (memberService *MemberService) GetAssets(ctx context.Context, memberID string) ([]Asset, error) {
+func (memberService *MemberService) GetAssets(ctx context.Context) (AssetList, error) {
 	resp, err := memberService.GET("/assets").Auth(memberService.Presign(time.Minute)).Do(ctx).Bytes()
 
 	if err != nil {
@@ -52,7 +91,7 @@ func (memberService *MemberService) GetAssets(ctx context.Context, memberID stri
 		return nil, err
 	}
 
-	var assets []Asset
+	var assets AssetList
 
 	err = jsoniter.Unmarshal(resp, &assets)
 
@@ -62,4 +101,89 @@ func (memberService *MemberService) GetAssets(ctx context.Context, memberID stri
 	}
 
 	return assets, nil
+}
+
+// GetAssetsByID 读取用户某币种余额
+func (memberService *MemberService) GetAssetsByID(ctx context.Context, assetID string) (AssetList, error) {
+	resp, err := memberService.GET("/assets").P("asset_id", assetID).Auth(memberService.Presign(time.Minute)).Do(ctx).Bytes()
+
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	var assets AssetList
+
+	err = jsoniter.Unmarshal(resp, &assets)
+
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return assets, nil
+}
+
+// GetSnapshots 读取转账记录列表
+func (memberService *MemberService) GetSnapshots(ctx context.Context, assetID string,
+	cursor string, limit int, order string) (SnapshotList, error) {
+	resp, err := memberService.GET("/snapshots").P("asset_id", assetID).P("cursor", cursor).P("limit", limit).P("order", order).Auth(memberService.Presign(time.Minute)).Do(ctx).Bytes()
+
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	var snapshots SnapshotList
+
+	err = jsoniter.Unmarshal(resp, &snapshots)
+
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return snapshots, nil
+}
+
+// Withdraw 提现
+func (memberService *MemberService) Withdraw(ctx context.Context) (Snapshot, error) {
+	resp, err := memberService.POST("/withdraw").Auth(memberService.Presign(time.Minute)).Do(ctx).Bytes()
+
+	if err != nil {
+		log.Error(err)
+		// return nil, err
+	}
+
+	var snapshot Snapshot
+
+	err = jsoniter.Unmarshal(resp, &snapshot)
+
+	if err != nil {
+		log.Error(err)
+		// return err
+	}
+
+	return snapshot, nil
+}
+
+// Transfer 转账
+func (memberService *MemberService) Transfer(ctx context.Context) (Snapshot, error) {
+	resp, err := memberService.POST("/transfer").Auth(memberService.Presign(time.Minute)).Do(ctx).Bytes()
+
+	if err != nil {
+		log.Error(err)
+		// return nil, err
+	}
+
+	var snapshot Snapshot
+
+	err = jsoniter.Unmarshal(resp, &snapshot)
+
+	if err != nil {
+		log.Error(err)
+		// return err
+	}
+
+	return snapshot, nil
 }
