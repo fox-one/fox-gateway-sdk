c1c7e592b7cf47051c0b7bed078e9375b8177c69
Zhiming Jiang <lanvige@gmail.com>
2019-01-26 01:04:34 +0800

    update testcase for wallet

--- wallet_test.go
+++ wallet_test.go
@@ -1,25 +1,21 @@
 package gateway
 
 import (
+	"context"
 	"testing"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	merchantKey    = "5c8a9491dca25af694004d5e1711b217"
-	merchantSecret = "64012120f9fb7daaa9f6ae48a159584d"
-	apiBase        = "https://dev-gateway.fox.one"
-	memberID       = "111"
-)
-
 func TestGetAssets(t *testing.T) {
 	log.SetLevel(log.DebugLevel)
 	merchantClient := NewMerchantClient(apiBase).WithSession(merchantKey, merchantSecret)
-	memberSvc := merchantClient.MemberService("payment", memberID)
+	memberSvc := merchantClient.MemberService("payment", "e0814259f9c34d58b010eb674049d883")
 
-	assets, err := memberSvc.GetAssets(ctx)
+	ctx := context.Background()
 
-	assert.NotEmpty(t, assets.Key)
+	assets, _ := memberSvc.GetAssets(ctx)
+
+	assert.NotEmpty(t, assets)
 }
